9fd9c8be4054354bb9abb3664f8987fce7dc51be
steven <Fernandezsteven185@gmail.com>
2023-07-12 07:43:35 -0500

    Home done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        typing: {
+          'from': { width: '0' },
+        },
+
+        blink: {
+          '50%': { border: 'transparent' },
+        }
+      },
+      colors: {
+        primary: '#EF4444 ',
+        primaryHover: '',
+      },
+
+      animation: {
+        typing: 'typing 1.5s steps(16)',
+        blink: 'blink .5s infinite step-end alternate',
+      }
+    },
   },
   plugins: [],
 }
